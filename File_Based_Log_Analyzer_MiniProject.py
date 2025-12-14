a6b0529a9f1db1e83004aa6033d84a2b83d50e6e
malith-jayashankha <ncmalithjayashankha@gmail.com>
2025-12-14 21:55:51 +0530

    list analyzer

---
+++ File_Based_Log_Analyzer_MiniProject.py
@@ -0,0 +1,29 @@
+complete_message = {"INFO":0,"WARNING":0,"ERROR":0}
+file_name = input("Enter file name: ")
+file_log = open(file_name,"r")
+
+lines_analyzed = 0
+
+for line in file_log:
+    lines_analyzed += 1
+    line_parts = line.split()
+    if line_parts:
+        log_type = line_parts[0].upper().replace(":","")
+        if log_type == "INFO":
+            complete_message["INFO"] += 1
+        elif log_type == "WARNING":
+            complete_message["WARNING"] += 1
+        elif log_type == "ERROR":
+            complete_message["ERROR"] += 1
+        else:
+            print(f"Skipping line {lines_analyzed}: Unknown log type '{log_type}' ")
+
+
+print(f"""
+==== LOG Summary ====
+Total Lines Analyzed : {lines_analyzed}
+INFO        : {complete_message["INFO"]}
+WARNING     : {complete_message["WARNING"]}
+ERROR       : {complete_message["ERROR"]}
+""")
+file_log.close()
